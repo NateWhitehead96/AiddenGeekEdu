fad1820ae2d5a344e497e3122aafd828f1c24c19
NateWhitehead96 <50887818+NateWhitehead96@users.noreply.github.com>
2022-02-07 21:33:40 -0500

    Added tilemap and level 1 design

---
+++ AiddenPlatformer/Assets/Scripts/Coin.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float moveSpeed;
+    public int direction = 1;
+    public float topBounds;
+    public float botBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        topBounds = transform.position.y + 0.5f; // top bounds
+        botBounds = transform.position.y - 0.5f; // bot bounds
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed * direction * Time.deltaTime); // move the coin
+
+        if(transform.position.y > topBounds)
+        {
+            direction = -1;
+        }
+        if(transform.position.y < botBounds)
+        {
+            direction = 1;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject); // for now just destroy when anything touches
+    }
+}
