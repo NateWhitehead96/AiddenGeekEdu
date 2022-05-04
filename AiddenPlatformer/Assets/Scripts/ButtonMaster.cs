f990306c6e6df0e18c69d5a73d93cbfec0867bca
NateWhitehead96 <50887818+NateWhitehead96@users.noreply.github.com>
2022-05-03 20:06:51 -0400

    Main Menu work

--- AiddenPlatformer/Assets/Scripts/ButtonMaster.cs
+++ AiddenPlatformer/Assets/Scripts/ButtonMaster.cs
@@ -19,6 +19,23 @@ public class ButtonMaster : MonoBehaviour
     public void ReturnToMainMenu()
     {
         print("Opening Main menu");
-        //SceneManager.LoadScene("MainMenu"); // load main menu
+        SceneManager.LoadScene("MainMenu"); // load main menu
+    }
+
+    public void PlayGame()
+    {
+        if (PlayerPrefs.HasKey("Lives")) // more to check if we have save data, Lives would be in that
+        {
+            ReturnToHub(); // load hubworld
+        }
+        else // no save data
+        {
+            SceneManager.LoadScene("TutorialLevel"); // load tutorial level
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // this closes the game, but only works in a standalone build (not in editor)
     }
 }
