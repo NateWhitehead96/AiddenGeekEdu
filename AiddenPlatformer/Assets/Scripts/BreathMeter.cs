70d44346bcffe0d98bda8fa0070b98a14da2f952
NateWhitehead96 <50887818+NateWhitehead96@users.noreply.github.com>
2022-06-02 20:38:22 -0400

    added water effects

--- AiddenPlatformer/Assets/Scripts/BreathMeter.cs
+++ AiddenPlatformer/Assets/Scripts/BreathMeter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering; // this allows us to use the Volume scripts
 
 public class BreathMeter : MonoBehaviour
 {
@@ -10,7 +11,8 @@ public class BreathMeter : MonoBehaviour
     // access to the new canvas that has our breath meter (or at least just the breath meter)
     public Slider Breath;
     // maybe the post processing volume for showing/hiding effects
-    public GameObject ppVolume; // optional right now
+    public Volume inWaterVolume; // the post processing volume for our scene in the water
+    public Volume outOfWaterVolume; // the post processing volume for our scene out of the water
 
     public float maxBreath;
     public float currentBreath;
@@ -31,15 +33,19 @@ public class BreathMeter : MonoBehaviour
             Breath.gameObject.SetActive(true); // show the breath meter
             currentBreath -= Time.deltaTime; // slowly melt our breath down
             Breath.value = currentBreath; // this will update the slider bar
+            inWaterVolume.weight = Mathf.Lerp(inWaterVolume.weight, 1, Time.deltaTime); // slowly transition to water volume
+            outOfWaterVolume.weight = Mathf.Lerp(outOfWaterVolume.weight, 0, Time.deltaTime);
             if(currentBreath <= 0)
             {
-                
+                // may fix this so it doesnt spam player damage
                 player.HurtPlayer();
             }
         }
         if(player.swimming == false)
         {
-            if(currentBreath <= maxBreath)
+            inWaterVolume.weight = Mathf.Lerp(inWaterVolume.weight, 0, Time.deltaTime); // slowly transition to ofw volume
+            outOfWaterVolume.weight = Mathf.Lerp(outOfWaterVolume.weight, 1, Time.deltaTime);
+            if (currentBreath <= maxBreath)
             {
                 currentBreath += 3 * Time.deltaTime;
                 Breath.value = currentBreath;
