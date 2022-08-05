7f07628d27081d5bd1cee69147d5de750c60ecec
NateWhitehead96 <50887818+NateWhitehead96@users.noreply.github.com>
2022-08-04 21:35:18 -0400

    boss work

--- AiddenPlatformer/Assets/Scripts/BossTrigger.cs
+++ AiddenPlatformer/Assets/Scripts/BossTrigger.cs
@@ -2,20 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.Playables;
 
 public class BossTrigger : MonoBehaviour
 {
-    public GameObject wall; // wall 1
+    public GameObject wall; // wall 
     public GameObject bossCanvas; // to show/hide boss canvas
     public bool fightingBoss; // to know we're passed through the trigger box and the walls will come up
     //public float shakeDuration; // how long to shake
     public float shakeMagnitude; // how aggressive the shake will be
-    public Vector3 initpos;
+    public PlayableDirector director; // the intro animation for our boss
+    public GameObject[] bossPieces; // all of the boss parts
+    // our new shake variables, we dont need to set them in the inspector so we'll make them all private
+    int direction = 1;
+    float left, right;
     // Start is called before the first frame update
     void Start()
     {
         bossCanvas.SetActive(false); // hide this stuff
-        initpos = wall.transform.position;
+        // set "left" and "right"
+        left = wall.transform.position.x - 0.1f;
+        right = wall.transform.position.x + 0.1f;
+        for (int i = 0; i < bossPieces.Length; i++)
+        {
+            bossPieces[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -23,8 +34,16 @@ public class BossTrigger : MonoBehaviour
     {
         if(fightingBoss == true && wall.transform.position.y < 0)
         {
-            wall.transform.localPosition = wall.transform.position + Random.insideUnitSphere * shakeMagnitude;
-            wall.transform.position = new Vector3(0, wall.transform.position.y + 1 * Time.deltaTime);
+            if(wall.transform.position.x > right)
+            {
+                direction = -1;
+            }
+            if(wall.transform.position.x < left)
+            {
+                direction = 1;
+            }
+            //wall.transform.localPosition = wall.transform.position + Random.insideUnitSphere * shakeMagnitude;
+            wall.transform.position = new Vector3(wall.transform.position.x + direction * 3 * Time.deltaTime, wall.transform.position.y + 2 * Time.deltaTime);
         }
             
            
@@ -34,8 +53,14 @@ public class BossTrigger : MonoBehaviour
     {
         if (collision.gameObject.GetComponent<Player>())
         {
+            for (int i = 0; i < bossPieces.Length; i++)
+            {
+                bossPieces[i].SetActive(true);
+            }
             fightingBoss = true;
             bossCanvas.SetActive(true);
+            director.Play();
         }
+        GetComponent<BoxCollider2D>().enabled = false; // disable the collider
     }
 }
