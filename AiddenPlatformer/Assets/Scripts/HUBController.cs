c8cbb915b1fc32de7b42f24f42936cb629fa7591
NateWhitehead96 <n_whitehead96@hotmail.ca>
2022-08-25 20:34:17 -0400

    Level linking

--- AiddenPlatformer/Assets/Scripts/HUBController.cs
+++ AiddenPlatformer/Assets/Scripts/HUBController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class HUBController : MonoBehaviour
 {
-    public GameObject[] barriers;
+    public GameObject[] barriers; // walls blocking levels 2-5
+    public GameObject[] doors; // the doors we have in our hub world
+    public Sprite openDoorSprite; // the sprite we turn our doors to
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +15,18 @@ public class HUBController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < barriers.Length; i++)
+        for (int i = 0; i < barriers.Length; i++) // to control what walls are up
         {
             if (GameManager.instance.LevelsBeaten > i + 1)
             {
                 barriers[i].SetActive(false);
+            }
+        }
+        for (int i = 0; i < doors.Length; i++) // to control what doors are open
+        {
+            if(GameManager.instance.LevelsBeaten > i)
+            {
+                doors[i].GetComponentInChildren<SpriteRenderer>().sprite = openDoorSprite;
             }
         }
     }
